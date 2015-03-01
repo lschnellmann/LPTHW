8e0190792c8a7cde3b7899e787f35dfdfba6acc7
lschnellmann <lschnellmann@gmail.com>
2015-03-01 15:01:28 -0600

    Create ex10drill.py

---
+++ ex10drill.py
@@ -0,0 +1,17 @@
+#drills to study % string characters and escape characters
+
+#what is the difference between %r and %s ?
+
+#variables
+
+print "What is your name?", 
+name = raw_input(), 
+print "what is your surname?", 
+last_name = raw_input(),
+print  'Name is  %s %s '  % (  name + last_name) 
+
+
+
+
+ 
+
