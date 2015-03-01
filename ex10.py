f8e5cfca03ba680a7845a68d10acf4445c12a6dc
lschnellmann <lschnellmann@gmail.com>
2015-03-01 14:59:30 -0600

    Create ex10.py

---
+++ ex10.py
@@ -0,0 +1,28 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = '''
+I'll do a list:
+\t* Cat food
+\t* Fishies 
+\t* Catnip\t* Grass\t 
+'''
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
+#while True:
+#	for i in ["/","-","|","\\","*|"]:
+#		print "%s\r" % i,
+
+print "\n by \r "
+print " \r Larry " 
+print "\r Schnellmann" 
+
+Larry = "%r"
+print Larry % (Larry)
+
+
