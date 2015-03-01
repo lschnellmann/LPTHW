ab8c7299659427e702e895f89a482b7099423de1
lschnellmann <lschnellmann@gmail.com>
2015-03-01 16:31:39 -0600

    Create ex21drill1.py

---
+++ ex21drill1.py
@@ -0,0 +1,26 @@
+#If you aren't really sure what return does, 
+#try writing a few of your own functions 
+#and have them return some values. You can return anything that you 
+#can put to the right of an =.
+
+
+#name and address function using the keyword 'return'
+
+
+firstName = (raw_input('Please enter your first name: '))
+lastName = (raw_input('Please enter your first name: '))
+	
+
+
+def lastFirst(firstName, lastName):
+    separator = ', '
+    result = lastName + separator + firstName
+    return result
+
+
+	
+
+
+
+	
+
