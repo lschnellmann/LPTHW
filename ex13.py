631b0596af43a188b8c316da3462bb6123bc457a
lschnellmann <lschnellmann@gmail.com>
2015-03-01 15:04:21 -0600

    Create ex13.py

---
+++ ex13.py
@@ -0,0 +1,19 @@
+# Excercise 13: Parameters, Unpacking, Variables
+# In this exercise we will cover one more input method you can use to pass variables to a script
+# (script being another name for your .py files).  You know how to type 'python.ex13.py' to run the ex13.py file?
+# Well the ex13.py part of the command is called an 'argument'.  What we'll do now is write a script
+# that also accepts arguments.
+
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
+s = raw_input("enter a variable ")
+print s
+s = argv 
+print "the argv variable is ", s
