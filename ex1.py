18eebb84a84528feeadd0fc93dfa1d0f2e90991e
lschnellmann <lschnellmann@gmail.com>
2015-03-01 06:53:46 -0600

    Create ex1.py
    from the first lesson in Learn Python the Hardway

---
+++ ex1.py
@@ -0,0 +1,26 @@
+
+howmany = int(raw_input("how many times to run script:? "))
+with open('ex1.py') as h:
+	i = 1
+	for howmany in h:
+		if h == howmany 
+yes_no = str(raw_input("Print just one line:? "))
+if yes_no == ('yes'):
+	line_number = int(raw_input("what line:? "))
+	with open('ex1.py') as f:
+		i = 1
+		for line in f:
+			if i == line_number:
+				break
+			i +=1
+	print line
+	if yes_no == ('yes'):
+		quit()
+	
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay!  Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
