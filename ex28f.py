d3eccfbf1f03836d55feeb62772dfefcec98481c
lschnellmann <lschnellmann@gmail.com>
2015-03-01 16:38:51 -0600

    Create ex28f.py
    trying to do this more effeciently with functions

---
+++ ex28f.py
@@ -0,0 +1,63 @@
+# the logic quiz redone using functions
+
+#THERE HAS TO BE A WAY TO DO THIS MORE EFFICIENTLY WITH FUNCTIONS.
+#_________________________________________________________________
+
+#first the imports.
+
+from random import randint;import time;import sys
+
+#**************FUNCTIONS********************************************
+# The clear screen function.. really it just scrolls up real fast.
+def clear(num):
+	for i in range (num):print
+	
+#assign variable q
+q = 0
+x = 0 
+	
+#Question Number Function....................................
+
+def Q_num(q):
+	q = q + 1
+	print("Question %i") % (q)	
+	
+	
+
+
+Q_num(x)
+
+print "\n "
+
+Q_num(x+1)
+
+print '\n'
+
+Q_num(x+2)
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
