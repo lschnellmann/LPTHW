4316eb5b06197054e3346b3f59c4e052fa46642d
lschnellmann <lschnellmann@gmail.com>
2015-03-01 16:40:01 -0600

    Create ex102.py

---
+++ ex102.py
@@ -0,0 +1,32 @@
+
+
+# functions that use strings, variables, integers and floats
+
+# a function that uses a string with those funny % things.
+
+def cat():
+	tabby_cat = "\tI'm tabbed in."
+	persian_cat = "I'm split\non a line."
+	backslash_cat = "I'm \\ a \\ cat."
+	fat_cat = """
+	I'll do a list:
+	\t* Cat food
+	\t* Fishies 
+	\t* Catnip\n\t* Grass
+	"""
+	print tabby_cat
+	print persian_cat
+	print backslash_cat
+	print fat_cat
+	
+cat()
+
+print """ I'll do another list:
+\t cat essentials
+\t* what does the asterisk do?
+oh, nothing, it was just part of making the list.
+
+It was the " \-t " we were interested in"""
+
+
+
