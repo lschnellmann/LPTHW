0d428c593d0c4d4570b207548bdc63bdbe94ead6
lschnellmann <lschnellmann@gmail.com>
2015-03-01 15:02:05 -0600

    Create ex12.py

---
+++ ex12.py
@@ -0,0 +1,10 @@
+# Exercise 12 : Prompting People
+
+#_____________________________________
+
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weigh? ")
+
+print "so, you're %r years old, %s tall and %r pounds." % (
+	age, height, weight)
