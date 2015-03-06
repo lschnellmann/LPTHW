197222aaead8c46129b1821237c10287e53e1275
lschnellmann <lschnellmann@gmail.com>
2015-03-05 22:55:34 -0600

    Rename return.py to LOGICQUIZ.py
    logic quiz from LPTHW.. can't figure out how to make it keep score.

---
+++ LOGICQUIZ.py
@@ -0,0 +1,360 @@
+
+def q1(answer): 
+				
+	if  answer == str("false"):
+		print "CORRECT, answer is ", 
+		y = (False)
+		print y 
+		print " \n "
+	else:
+		print "WRONG, answer is ---> ",
+		x = (not True)
+		print x;print " \n "
+		
+
+def q2(answer2):
+	
+	if answer2 == str("true"):
+		print "CORRECT, answer is ",
+		y = (True)
+		print y 
+		print " \n "
+	else:
+		print "WRONG, answer is ---> ",
+		x = (True)
+		print x;print " \n "
+		
+def q3(answer3):
+	if answer3 == str("true"):
+		print "CORRECT, answer is ", 
+		y = (True or False)
+		print y
+		print " \n "
+	else:
+		y = (True or False)
+		print "Wrong, answer is ---> ",
+		print y
+		print " \n "
+		
+def q4(answer4):
+	
+	if answer4 == str("true"):
+		print "CORRECT, answer is ",
+		y = (True or True)
+		print y
+		print " \n "
+	else:
+		y = (True or True)
+		print "Wrong, answer is ---> ",
+		print y
+		print " \n "
+		
+def q5(answer5):
+	
+	if answer5 == str("true"):
+		print "CORRECT, answer is ",
+		y = (False or True)
+		print y
+		print " \n "
+	else:
+		y = (False or True)
+		print "Wrong, answer is ---> ",
+		print y
+		print " \n "
+		
+def q6(answer6):
+	print 
+	if answer6 == str("false"):
+		print "CORRECT, answer is ",
+		y = (False or False)
+		print y
+		print " \n "
+	else:
+		y = (False or False)
+		print "Wrong, answer is ---> ",
+		print y
+		print " \n "
+		
+def q7(answer7):
+	if answer7 == str("false"):
+		print "CORRECT, answer is ",
+		y = (True and False)
+		print y 
+		print " \n "
+	else:
+		y = (True and False)
+		print "Wrong, answer is ---> ",
+		print y
+		print " \n "
+		
+def q8(answer8):
+	
+	if answer8 == str("true"):
+		print "CORRECT, answer is ",
+		y = (True and True)
+		print y 
+		print " \n "
+	else:
+		y = (True and True)
+		print "Wrong, answer is ---> ",
+		print y
+		print " \n "
+		
+def q9(answer9):
+	
+	if answer9 == str("false"):
+		print "CORRECT, answer is ",
+		y = (False and True)
+		print y 
+		print " \n "
+	else:
+		y = (False and True)
+		print "Wrong, answer is ---> ",
+		print y
+		print " \n "
+		
+def q10(answer10):
+	
+	if answer10 == str("false"):
+		print "CORRECT, answer is ",
+		y = (False and False)
+		print y 
+		print " \n "
+	else:
+		y = (False and False)
+		print "Wrong, answer is ---> ",
+		print y
+		print " \n "
+		
+def q11(answer11):
+	if answer11 == str("false"):
+		print "CORRECT, answer is ",
+		y = not (True or False)
+		print y 
+		print " \n "
+	else:
+		y = not (True or False)
+		print "Wrong, answer is ---> ",
+		print y
+		print " \n "
+		
+def q12(answer12):
+	
+	if answer12 == str("false"):
+		print "CORRECT, answer is ",
+		y = not (True or True)
+		print y 
+		print " \n "
+	else:
+		y = not (True or True)
+		print "Wrong, answer is ---> ",
+		print y
+		print " \n "
+		
+def q13(answer13):
+	
+	if answer13 == str("false"):
+		print "CORRECT, answer is ",
+		y = not (False or True)
+		print y 
+		print " \n "
+	else:
+		y = not (False or True)
+		print "Wrong, answer is ---> ",
+		print y
+		print " \n "
+		
+def q14(answer14):
+	
+	if answer14 == str("true"):
+		print "CORRECT, answer is ",
+		y = not(False or False)
+		print y 
+		print " \n "
+	else:
+		y = not(False or False)
+		print "Wrong, answer is ---> ",
+		print y
+		print " \n "
+		
+def q15(answer15):
+	if answer15 == str("true"):
+		print "CORRECT, answer is ",
+		y = not(True and False)
+		print y 
+		print " \n "
+	else:
+		y = not(True and False)
+		print "Wrong, answer is ---> ",
+		print y
+		print " \n "
+		
+def q16(answer16):
+	
+	if answer16 == str("false"):
+		print "CORRECT, answer is ",
+		y = not(True and True)
+		print y 
+		print " \n "
+	else:
+		y = not(True and True)
+		print "Wrong, answer is ---> ",
+		print y
+		print " \n "
+		
+def q17(answer17):
+	print 
+	if answer17 == str("true"):
+		print "CORRECT, answer is ",
+		y = not(False and True)
+		print y 
+		print " \n "
+	else:
+		y = not(False and True)
+		print "Wrong, answer is ---> ",
+		print y
+		print " \n "
+		
+def q18(answer18):
+	
+	if answer18 == str("true"):
+		print "CORRECT, answer is ",
+		y = not(False and False)
+		print y 
+		print " \n "
+	else:
+		y = not(False and False)
+		print "Wrong, answer is ---> ",
+		print y
+		print " \n "
+		
+def q19(answer19):
+	if answer19 == str("false"):
+		print "CORRECT, answer is ",
+		y = (1 == 0)
+		print y 
+		print " \n "
+	else:
+		y = (1 == 0)
+		print "Wrong, answer is ---> ",
+		print y
+		print " \n "
+		
+def q20(answer20):
+	
+	if answer20 == str("true"):
+		print "CORRECT, answer is ",
+		y = (1 == 1)
+		print y 
+		print " \n "
+	else:
+		y = (1 == 0)
+		print "Wrong, answer is ---> ",
+		print y
+		print " \n "
+		
+def q21(answer21):
+	
+	if answer21 == str("false"):
+		print "CORRECT, answer is ",
+		y = (0 == 1)
+		print y 
+		print " \n "
+	else:
+		y = (0 == 1)
+		print "Wrong, answer is ---> ",
+		print y
+		print " \n "
+		
+def q22(answer22):
+	
+	if answer22 == str("true"):
+		print "CORRECT, answer is ",
+		y = (0 == 0)
+		print y 
+		print " \n "
+	else:
+		y = (0 == 0)
+		print "Wrong, answer is ---> ",
+		print y
+		print " \n "
+		
+
+right = 0 ; wrong = 0
+right = (right + 1)
+wrong = (wrong + 1)
+	
+c = 0;c = (c + 1)
+
+
+	
+
+print "_________________|NOT.....|.....TRUE?______________________\n\n"
+	
+print "%i)" % (c), ;c = (c+1);answer = raw_input("Not True is ?").strip()
+q1(answer) 	
+print "%i)" % (c), ;c = (c+1);answer2 = raw_input("Not False is ?").strip()
+q2(answer2)
+print "_________________|OR.....|.....TRUE?_______________________\n\n"
+print "%i)" % (c), ;c = (c+1);answer3 = raw_input("True or False is ?").strip()
+q3(answer3)
+print "%i)" % (c), ;c = (c+1);answer4 = raw_input("True or True is ?").strip()
+q4(answer4)
+print "%i)" % (c), ;c = (c+1);answer5 = raw_input("False or True is ?").strip()
+q5(answer5)
+print "%i)" % (c), ;c = (c+1);answer6 = raw_input("False or False is ?").strip()
+q6(answer6)
+print "_________________|AND.....|.....TRUE?______________________\n\n"
+print "%i)" % (c), ;c = (c+1);answer7 = raw_input("True and False is ?").strip()
+q7(answer7)
+print "%i)" % (c), ;c = (c+1);answer8 = raw_input("True and True is ?").strip()
+q8(answer8)
+print "%i)" % (c), ;c = (c+1);answer9 = raw_input("False and True is ?").strip()
+q9(answer9)
+print "%i)" % (c), ;c = (c+1);answer10 = raw_input("False and False is ?").strip()
+q10(answer10)
+print "_________________|NOT OR..|.....TRUE?______________________\n\n"
+print "%i)" % (c), ;c = (c+1);answer11 = raw_input("not (True or False) is ?").strip()
+q11(answer11)
+print "%i)" % (c), ;c = (c+1);answer12 = raw_input("not (True or True) is ?").strip()
+q12(answer12)
+print "%i)" % (c), ;c = (c+1);answer13 = raw_input("not (False or True) is ?").strip()
+q13(answer13)
+print "%i)" % (c), ;c = (c+1);answer14 = raw_input("not (False or False) is ?").strip()
+q14(answer14)
+print "_________________|NOT AND..|.....TRUE?_____________________\n\n"
+print "%i)" % (c), ;c = (c+1);answer15 = raw_input("not (True and False) is ?").strip()
+q15(answer15)
+print "%i)" % (c), ;c = (c+1);answer16 = raw_input("not (True and True) is ?").strip()
+q16(answer16)
+print "%i)" % (c), ;c = (c+1);answer17 = raw_input("not (False and True) is ?").strip()
+q17(answer17)
+print "%i)" % (c), ;c = (c+1);answer18 = raw_input("not (False and False) is ?").strip()
+q18(answer18)
+print "_________________|..==..|.....TRUE?________________________\n\n"
+print "%i)" % (c), ;c = (c+1);answer19 = raw_input("\t 1 == 0 is ?").strip()
+q19(answer19)
+print "%i)" % (c), ;c = (c+1);answer20 = raw_input("\t 1 == 1 is ?").strip()
+q20(answer20)
+print "%i)" % (c), ;c = (c+1);answer21 = raw_input("\t 0 == 1 is ?").strip()
+q21(answer21)
+print "%i)" % (c), ;c = (c+1);answer22 = raw_input("\t 0 == 0 is ?").strip()
+q22(answer22)
+
+
+
+
+
+	
+	
+
+
+
+
+
+
+
+
+
+
+	
